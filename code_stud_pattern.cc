9fe4e703f1b4cbad013325619efd2cd94ee16935
ShantakshKamat <85739236+ShantakshKamat@users.noreply.github.com>
2022-12-09 00:14:23 +0530

    Create code_stud_pattern.cc

---
+++ code_stud_pattern.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h> 
+
+void printPattern(int n) {
+	for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<"1";
+        }
+        cout<<endl;
+    }
+        
+}
