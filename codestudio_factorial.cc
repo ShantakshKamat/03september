5f2f0cdb4b7cbe67527a238b74ec9bb712f8b5ae
ShantakshKamat <85739236+ShantakshKamat@users.noreply.github.com>
2022-12-08 23:59:30 +0530

    Update codestudio_factorial.cc

--- codestudio_factorial.cc
+++ codestudio_factorial.cc
@@ -1,20 +1,27 @@
-    
 #include <iostream>
 using namespace std;
 
 int main() {
       int n;
       cin>>n;
-      if(n>0){
-          for(int i=n-1; i>=1; i--)
-          {
-              n=n*i;
-          }
-             cout<<n;
-      }else if(n<=0){
-          cout<<1;
-      }    
-             
+      if(n < 0) {
+           cout<<"Error";
+       }
+       
+       else if(n == 0) {
+           cout<<1;
+       }
+       
+       else {
+           int i = n;
+           int fact = 1;
+           while(n / i != n) {
+               fact = fact * i;
+               i--;
+           }
+           
+           cout<<fact;
+       }
 
    return 0;
 }
