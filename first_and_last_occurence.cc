869867e53c11eab6b02fc4a8480516ff2191a3f4
ShantakshKamat <85739236+ShantakshKamat@users.noreply.github.com>
2023-02-13 23:58:53 +0530

    changes

--- first_and_last_occurence.cc
+++ first_and_last_occurence.cc
@@ -1,9 +1,11 @@
-int firstocc(vector<int> &arr, int n, int key)
+int firstocc(vector<int>& arr, int n, int key)
 {
-    int s = 0, e = n - 1;
+    int s = 0;
+    int e = n - 1;
+    int ans = -1;
 
     int mid = s + (e - s) / 2;
-    int ans = -1;
+
     while (s <= e)
     {
         if (arr[mid] == key)
@@ -12,11 +14,11 @@ int firstocc(vector<int> &arr, int n, int key)
             e = mid - 1;
         }
         else if (arr[mid] > key)
-        {
+        { // left jao
             e = mid - 1;
         }
-        else if (arr[mid] < key)
-        {
+        else
+        { // right mein jao
             s = mid + 1;
         }
         mid = s + (e - s) / 2;
@@ -24,13 +26,14 @@ int firstocc(vector<int> &arr, int n, int key)
     return ans;
 }
 
-
-int lastocc(vector<int> &arr, int n, int key)
+int lastocc(vector<int>& arr, int n, int key)
 {
-    int s = 0, e = n - 1;
+    int s = 0;
+    int e = n - 1;
+    int ans = -1;
 
     int mid = s + (e - s) / 2;
-    int ans = -1;
+
     while (s <= e)
     {
         if (arr[mid] == key)
@@ -39,23 +42,30 @@ int lastocc(vector<int> &arr, int n, int key)
             s = mid + 1;
         }
         else if (arr[mid] > key)
-        {
+        { // left jao
             e = mid - 1;
         }
-        else if (arr[mid] < key)
-        {
+        else
+        { // right mein jao
             s = mid + 1;
         }
         mid = s + (e - s) / 2;
     }
     return ans;
 }
-pair<int, int> findFirstLastPosition(vector<int> &arr, int n, int x)
+
+
+
+
+#include <bits/stdc++.h> 
+pair<int, int> firstAndLastPosition(vector<int>& arr, int n, int k)
 {
-	pair<int,int>p;
-    p.first=firstocc(arr,n,x);
-    p.second=lastocc(arr,n,x);
-    
+    // Write your code here
+
+    pair<int,int> p;
+
+    p.first=firstocc(arr,n,k);
+    p.second=lastocc(arr,n,k);
+
     return p;
 }
-
